d253eec4a7327ad5757d156ab0bb2c2f6e6c3e6a
Javier Alcala <javier.alcalaw@gmail.com>
2025-06-27 11:55:08 -0300

    Hice migraciones y corregí error de modelos (cambié models.model a models.Model)

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
-class Perfil(models.model):
+class Perfil(models.Model):
     usuario = models.CharField(max_length=100)
     biografia = models.TextField()
 
-class Post(models.model):
+class Post(models.Model):
     autor = models.CharField(max_length=100)
     contenido = models.TextField()
     fechaDeCreacion = models.DateTimeField(auto_now_add=True)
 
-class Comentario(models.model):
+class Comentario(models.Model):
     autor = models.CharField(max_length=100)
     contenido = models.TextField()
     fechaDeCreacion = models.DateTimeField(auto_now_add=True)
